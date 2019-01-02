052e96323ebb70669068a3a76f4056040b64df20
Sen Yang <yangsen@fb.com>
2019-01-02 12:19:03 -0800

    FCR Fulfill Facebook opensource requirements
    Summary:
    As title. See T35205910 and T37994370 for more context
    1. Add missing code of conduct file https://fburl.com/wiki/ya7kugey
    2. Add missing copy right header https://fburl.com/wiki/rdos028y
    3. Change version requirement of urllib3 to 1.23
    
    Reviewed By: starskywongfb
    
    Differential Revision: D13542414
    
    fbshipit-source-id: f1448aa5c28dcd4658b9dae85ecec635992187ff

--- fbnet/command_runner/ssh_netconf.py
+++ fbnet/command_runner/ssh_netconf.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 import asyncio
 import re
 
