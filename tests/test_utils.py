ce6f658f1d144a73dc20dfda2241d74c71567ea1
Sen Yang <yangsen@fb.com>
2019-02-19 12:02:09 -0800

    FCR support include TL1 response header in json output
    Summary: This is to have FCR adopt the the change in D13910504. This diff exposed an option in FCR so that user can choose to include the response message header in the json-formatted TL1 response
    
    Differential Revision: D13925511
    
    fbshipit-source-id: 56654415776d70f0ee6b0df6ec7182d4df5c3e1d

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+
+from fbnet.command_runner.utils import canonicalize
+
+
+class CanonicalizeTest(unittest.TestCase):
+    def test_canonicalize(self):
+        self.assertEqual(canonicalize("abc"), b"abc")
+        self.assertEqual(canonicalize(b"abc"), b"abc")
+        self.assertEqual(canonicalize(["abc", "xyz", b"123"]), [b"abc", b"xyz", b"123"])
