ab8c30ac8c7442830c8a2b9a6ee48746474d9624
Megan Yu <meganyu@fb.com>
2021-06-14 18:30:47 -0700

    Create decorator function to converts to thrift exceptions
    Summary:
    Diff Motivation:
    - problem: FCR currently raises many different kinds of exceptions and does not have a clear way to distinguish between device failures and FCR failures
    - solution: unify FCR exceptions, and classify exception types raised from FCR by assigning each an error code
    - this allows us to later export the error code to the Thrift Usage Scuba table, and more easily classify errors (e.g. device failure vs FCR failure)
    - In this diff, we want to create a decorator function that will later be used to wrap around all FCR Thrift API
    
    What this diff does:
    - adds a decorator function, ensure_thrift_exception, in exceptions.py that catches both FcrBaseExceptions and generic exceptions and converts to thrift exceptions to raise
    - adds test_ensure_thrift_exception unit test to test_exceptions.py that tests the new decorator
    - this diff does not actually use the decorator function in production code
    
    Differential Revision: D29080223
    
    fbshipit-source-id: bb0d01192725a8bc728d0552757f0afd882c7279

--- fbnet/command_runner/exceptions.py
+++ fbnet/command_runner/exceptions.py
@@ -6,7 +6,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import ClassVar
+from functools import wraps
+from typing import ClassVar, Callable, Any, TypeVar, cast
 
 from fbnet.command_runner_asyncio.CommandRunner import ttypes as fcr_ttypes
 from fbnet.command_runner_asyncio.CommandRunner.ttypes import FcrErrorCode
@@ -58,3 +59,39 @@ class DeviceErrorException(FcrBaseException):
 
 class ConnectionErrorException(FcrBaseException):
     _CODE: ClassVar[FcrErrorCode] = FcrErrorCode.CONNECTION_ERROR
+
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+def ensure_thrift_exception(fn: F) -> F:
+    """
+    Catch all FcrBaseExceptions and generic exception
+    Convert to Thrift SessionException and raise
+    """
+
+    @wraps(fn)
+    async def wrapper(self, *args, **kwargs):
+        try:
+            return await fn(self, *args, **kwargs)
+        except Exception as e:
+            if isinstance(e, fcr_ttypes.SessionException):
+                raise e
+
+            fcr_ex: fcr_ttypes.SessionException
+            if isinstance(e, FcrBaseException):
+                fcr_ex = await e.to_thrift_exception()
+            elif isinstance(e, PermissionError):
+                # use str(e) for known exceptions to keep exception messages clean
+                fcr_ex = await PermissionErrorException(str(e)).to_thrift_exception()
+            elif isinstance(e, ValueError):
+                fcr_ex = await ValueErrorException(str(e)).to_thrift_exception()
+            elif isinstance(e, RuntimeError):
+                fcr_ex = await RuntimeErrorException(str(e)).to_thrift_exception()
+            else:
+                # use repr(e) for unknown exceptions to preserve exception information
+                fcr_ex = await UnknownException(repr(e)).to_thrift_exception()
+
+            raise fcr_ex
+
+    return cast(F, wrapper)
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -6,7 +6,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Type
+from typing import List, Type, Dict
 
 from fbnet.command_runner.exceptions import (
     FcrBaseException,
@@ -17,13 +17,16 @@ from fbnet.command_runner.exceptions import (
     RuntimeErrorException,
     DeviceErrorException,
     ConnectionErrorException,
+    ensure_thrift_exception,
 )
 from fbnet.command_runner_asyncio.CommandRunner import ttypes as fcr_ttypes
+from fbnet.command_runner_asyncio.CommandRunner.ttypes import FcrErrorCode
 
 from .testutil import AsyncTestCase, async_test
 
 
 class ExceptionTest(AsyncTestCase):
+    # Defined FcrBaseException exceptions
     FCR_EXCEPTIONS: List[Type[FcrBaseException]] = [
         UnknownException,
         ValidationErrorException,
@@ -33,6 +36,13 @@ class ExceptionTest(AsyncTestCase):
         DeviceErrorException,
         ConnectionErrorException,
     ]
+
+    # Other non-FcrBaseException exceptions that FCR knows about
+    KNOWN_EXCEPTIONS: Dict[Type[Exception], FcrErrorCode] = {
+        PermissionError: FcrErrorCode.PERMISSION_ERROR,
+        ValueError: FcrErrorCode.VALUE_ERROR,
+        RuntimeError: FcrErrorCode.RUNTIME_ERROR,
+    }
 
     @async_test
     async def test_to_thrift_exception(self) -> None:
@@ -50,3 +60,69 @@ class ExceptionTest(AsyncTestCase):
             self.assertIsInstance(converted_exc, fcr_ttypes.SessionException)
             self.assertEqual(exc._CODE, converted_exc.code)
             self.assertEqual(str(exc), converted_exc.message)
+
+    @async_test
+    async def test_ensure_thrift_exception(self) -> None:
+        """
+        Test for the ensure_thrift_exception decorator
+        The decorator should catch all exceptions and convert to Thrift exceptions
+        """
+
+        @ensure_thrift_exception
+        async def test_raise_no_exception(self, return_msg: str) -> str:
+            return return_msg
+
+        @ensure_thrift_exception
+        async def test_raise_exception(self, exc: Exception, return_msg: str) -> str:
+            raise exc
+            return return_msg
+
+        return_msg = "A returned string"
+
+        # Test normal case: no exception raised
+        try:
+            res = await test_raise_no_exception(self, return_msg=return_msg)
+            self.assertEqual(res, return_msg)
+        except Exception:
+            self.fail("test_raise_no_exception raised an exception")
+
+        # Test that FcrBaseException types are converted
+        for exception_type in self.FCR_EXCEPTIONS:
+            exc = exception_type("This is an FCR exception!")
+
+            # Make sure that the exception is Thrift-defined now
+            with self.assertRaises(fcr_ttypes.SessionException) as context:
+                res = await test_raise_exception(self, exc=exc, return_msg=return_msg)
+
+            converted_exc = context.exception
+            self.assertIsInstance(converted_exc, fcr_ttypes.SessionException)
+            self.assertEqual(exc._CODE, converted_exc.code)
+            self.assertEqual(str(exc), converted_exc.message)
+
+        # Test that all other known Exceptions are converted
+        for exception_type in self.KNOWN_EXCEPTIONS.keys():
+            exc = exception_type("This is a known exception!")
+
+            # Make sure that the exception is Thrift-defined now
+            with self.assertRaises(fcr_ttypes.SessionException) as context:
+                res = await test_raise_exception(self, exc=exc, return_msg=return_msg)
+
+            converted_exc = context.exception
+            self.assertIsInstance(converted_exc, fcr_ttypes.SessionException)
+            self.assertEqual(self.KNOWN_EXCEPTIONS[exception_type], converted_exc.code)
+            print(converted_exc.message)
+            self.assertEqual(str(exc), converted_exc.message)
+
+        # Test that all unknown Exceptions are converted
+        unknown_exc = Exception("This is an unknown exception!")
+
+        # Make sure that the exception is Thrift-defined now
+        with self.assertRaises(fcr_ttypes.SessionException) as context:
+            res = await test_raise_exception(
+                self, exc=unknown_exc, return_msg=return_msg
+            )
+
+        converted_exc = context.exception
+        self.assertIsInstance(converted_exc, fcr_ttypes.SessionException)
+        self.assertEqual(FcrErrorCode.UNKNOWN, converted_exc.code)
+        self.assertEqual(repr(unknown_exc), converted_exc.message)
