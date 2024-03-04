de14a018a658e653ea3e473af2851b3b402730cf
Conner Nilsen <connernilsen@meta.com>
2024-03-04 13:48:45 -0800

    Pyre Configurationless migration for] [batch:82/244]
    Reviewed By: inseokhwang
    
    Differential Revision: D54470443
    
    fbshipit-source-id: fec83f82f94e8a56b39dfb64639c08e8bd0bbad5

--- fbnet/command_runner/options.py
+++ fbnet/command_runner/options.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# pyre-unsafe
+
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # This source code is licensed under the MIT license found in the
